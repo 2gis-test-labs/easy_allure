8fc87dcd254634ccb4ef57a646fc87712da1e4aa
b.polyanok <b.polyanok@2gis.ru>
2022-03-25 15:29:45 +0300

    add executables for each platform

---
+++ easy_allure/allurectl.py
@@ -0,0 +1,22 @@
+import pkg_resources
+import platform
+
+
+allure_executables = {
+    'Darwin': {
+        'x86_64': 'lib/allurectl_darwin_amd64'
+    },
+    'Linux': {
+        'arm': 'lib/allurectl_linux_arm64',
+        'i386': 'lib/allurectl_linux_386',
+        'x86_64': 'lib/allurectl_linux_amd64'
+    },
+    'Windows': {
+        'x86_64': 'lib/allurectl_windows_amd64.exe',
+        'arm': 'lib/allurectl_windows_arm64.exe',
+    }
+}
+
+def get_allure_executable():
+    executable = allure_executables[platform.system()][platform.machine()]
+    return pkg_resources.resource_filename('easy_allure', executable)
