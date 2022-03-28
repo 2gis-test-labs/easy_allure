4bc934915c0fdf042f95197793b94730880d902c
b.polyanok <b.polyanok@2gis.ru>
2022-03-28 10:30:57 +0300

    remove prints

--- easy_allure/helpers.py
+++ easy_allure/helpers.py
@@ -9,8 +9,6 @@ def run_cmd(cmd: str, timeout: int = 60) -> Tuple[str, str]:
                             universal_newlines=True,
                             shell=True)
     stdout, stderr = proc.communicate(timeout=timeout)
-    print(stdout)
-    print(stderr)
     if proc.returncode != 0:
         raise RuntimeError('Failed to run <{}>, got {}'.format(cmd, stderr))
     return stdout, stderr
